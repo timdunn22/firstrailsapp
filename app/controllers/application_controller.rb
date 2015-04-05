d4acd4f6a18ae7bc2508b9c3901b02170af19f71
Tim Dunn <timdunn2@gmail.com>
2015-04-05 18:50:50 +0545

    authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Pundit
   protect_from_forgery with: :exception
   before_action :flash_attack, if: :devise_controller?
 
+     rescue_from Pundit::NotAuthorizedError do |exception|
+       redirect_to root_url, alert: exception.message
+     end
    protected
 
    def flash_attack
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+ def admin?
+   role == 'admin'
+ end
+
+ def moderator?
+   role == 'moderator'
+ end
 end
