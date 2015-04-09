d1498ba16bd6cf85db3e496b118095cac7e4487b
Tim Dunn <timdunn2@gmail.com>
2015-04-09 22:39:18 +0545

    short production env change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'tim-bloccit.herokuapp.com' }
   config.force_ssl = true
 end
