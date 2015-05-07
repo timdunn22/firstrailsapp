05bedbabdc447d401213382994b80a1add56457c
Tim Dunn <timdunn2@gmail.com>
2015-05-06 17:26:46 -0700

    fixed markdown stuff

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,16 +12,16 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
-  
+
 
  def markdown_title
 
-   render_as_markdown(@post.title)
+   render_as_markdown(title)
 
  end
 
  def markdown_body
-   render_as_markdown(@post.body)
+   render_as_markdown(body)
  end
 
  private
