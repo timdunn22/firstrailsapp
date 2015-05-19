5dc06e405bd9ba631fe3c66c2ce3bc16d721425d
Tim Dunn <timdunn2@gmail.com>
2015-05-19 12:08:26 -0700

    changed scoping

---
+++ spec/models/topic_spec.rb
@@ -0,0 +1,36 @@
+describe Topic do
+  describe "scopes" do
+
+    before do
+      @public_topic = Topic.create # default is public
+      @private_topic = Topic.create(public: false)
+    end
+
+    describe "publicly_viewable" do
+      it "returns a relation of all public topics" do
+        expect(Topic.publicly_viewable).to eq( [@public_topic] )
+      end
+    end
+
+    describe "privately_viewable" do
+      it "returns a relation of all private topics" do
+        expect(Topic.privately_viewable).to eq([@private_topic])
+        # Your code here
+      end
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = true # sneaky solution; we don't need a real user, just something truthy
+        expect(Topic.visible_to(user)).to eq(Topic.all) if user.present?
+        # Your code here
+      end
+
+      it "returns only public topics if user is nil" do
+
+        expect(Topic.visible_to(User.first)).to eq(Topic.publicly_viewable) if User.first == nil
+        # Your code here
+      end
+    end
+  end
+end
