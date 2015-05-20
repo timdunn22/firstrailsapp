04523b040cb0d7cb937effe4052a2f2f79163c0e
Tim Dunn <timdunn2@gmail.com>
2015-05-19 17:38:51 -0700

    other interlude stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,20 @@ module ApplicationHelper
      content_tag :div, capture(&block), class: 'form-group'
    end
  end
+ def up_vote_link_classes(post)
+   if current_user.voted(post) && current_user.voted(post).up_vote?
+     "glyphicon glyphicon-chevron-up voted"
+   else
+     "glyphicon glyphicon-chevron-up"
+   end
+ end
 
+ def down_vote_link_classes(post)
+   if current_user.voted(post) && current_user.voted(post).down_vote?
+     "glyphicon glyphicon-chevron-down voted"
+   else
+     "glyphicon glyphicon-chevron-down"
+   end
+
+ end
 end
