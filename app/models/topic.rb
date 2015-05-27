91f66b80c03acfa465557a46f908b76882b00cb5
Tim Dunn <timdunn2@gmail.com>
2015-05-27 16:38:31 -0700

    private topics specs

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,8 @@ class Topic < ActiveRecord::Base
 
    has_many :posts, dependent: :destroy
   validates :name, length: { minimum: 5 }, presence: true
+  scope :visible_to, -> (user) { user ? all : publicly_viewable }
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
   self.per_page = 50
 end
