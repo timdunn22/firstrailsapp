e633471d97ca183e71ed3beb82354a99edc093c6
Tim Dunn <timdunn2@gmail.com>
2015-06-01 12:53:09 -0700

    new spec

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -23,16 +23,22 @@ require 'rails_helper'
        expect( page ).to have_content(@comment.body)
 
      end
+   end
+   before do
+     login_as(@user, :scope => :user)
+   end
+
+  describe "signed in" do
+
+    it "shows current user's profile" do
+
+      visit user_path(@user)
+
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
   end
-  # describe "signed in" do
-  #
-  #   it "shows current user's profile"
-  #   expect(current_path).to eq(user_path(@user))
-  #
-  #   expect( page ).to have_content(@user.name)
-  #   expect( page ).to have_content(@post.title)
-  #   expect( page ).to have_content(@comment.body)
-  #   end
-  #
-  #  end
  end
